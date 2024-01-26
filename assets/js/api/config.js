39b70894eac07435bba140e9563df7c5cdfc1b9c
Imaad08 <imaadmuzaffer@gmail.com>
2024-01-26 08:57:09 -0800

    fixed again pls work

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,8 +1,8 @@
 export var uri;
 if (location.hostname === "localhost") {
-        uri = "http://localhost:8086";
+        uri = "http://localhost:8762";
 } else if (location.hostname === "127.0.0.1") {
-        uri = "http://127.0.0.1:8086";
+        uri = "http://127.0.0.1:8762";
 } else {
         uri = "https://flask2.nighthawkcodingsociety.com";
 }
